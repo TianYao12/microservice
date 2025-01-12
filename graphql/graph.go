56657e88332becbb0dbf73576e38fbbfdf5c4d67
Tian <xtyao2015@gmail.com>
2025-01-11 21:23:42 -0500

    add order stuff

--- graphql/graph.go
+++ graphql/graph.go
@@ -3,34 +3,39 @@ package main
 import (
 	"github.com/99designs/gqlgen/graphql"
 	"github.com/TianYao12/microservice/account"
+	"github.com/TianYao12/microservice/catalog"
+	"github.com/TianYao12/microservice/order"
 )
 
 type Server struct {
-	accountClient		*account.Client
-	catalogClient		*catalog.Client
-	orderClient			*order.Client
+	accountClient *account.Client
+	catalogClient *catalog.Client
+	orderClient   *order.Client
 }
 
-func NewGraphQLServer(accountUrl, catalogUrl, orderUrl string) (*Server, error) {
+func NewGraphQLServer(accountUrl, catalogURL, orderURL string) (*Server, error) {
+	// Connect to account service
 	accountClient, err := account.NewClient(accountUrl)
 	if err != nil {
 		return nil, err
 	}
 
-	catalogClient, err := catalog.NewClient(catalogUrl)
+	// Connect to product service
+	catalogClient, err := catalog.NewClient(catalogURL)
 	if err != nil {
 		accountClient.Close()
 		return nil, err
 	}
 
-	orderClient, err := order.NewClient(orderUrl)
+	// Connect to order service
+	orderClient, err := order.NewClient(orderURL)
 	if err != nil {
 		accountClient.Close()
 		catalogClient.Close()
 		return nil, err
 	}
 
-	return &Server {
+	return &Server{
 		accountClient,
 		catalogClient,
 		orderClient,
@@ -43,14 +48,20 @@ func (s *Server) Mutation() MutationResolver {
 	}
 }
 
-func (s *Server) Account() AccountResolver{
+func (s *Server) Query() QueryResolver {
+	return &queryResolver{
+		server: s,
+	}
+}
+
+func (s *Server) Account() AccountResolver {
 	return &accountResolver{
 		server: s,
 	}
 }
 
 func (s *Server) ToExecutableSchema() graphql.ExecutableSchema {
-	return NewExecutableSchema(Config {
-		Resolvers:s,
+	return NewExecutableSchema(Config{
+		Resolvers: s,
 	})
 }
